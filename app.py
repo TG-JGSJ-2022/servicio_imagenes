f84922569907b2a89eadc611846dc4d56ff24428
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2022-02-06 22:20:34 -0500

    Endpoint created

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return "hola mundo"
 
+@app.route("/resize")
+def resize_image():
+    return "Resize image"
+
 app.run(debug=True,
         port = 5000,
         host="0.0.0.0")
